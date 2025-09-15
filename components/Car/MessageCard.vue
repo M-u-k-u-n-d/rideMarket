de33148329f5618e5758aaf32e6c099e6c01b62b
Mukund Tiwari <mukundtiwari251103@gmail.com>
2025-09-15 12:24:49 +0530

    updated messagecard

--- components/Car/MessageCard.vue
+++ components/Car/MessageCard.vue
@@ -8,14 +8,14 @@ defineProps({
 </script>
 
 <template>
-  <div class="even:bg-gray-100 p-4">
-    <h1 class="font-fold text-2xl">{{ message.name }}</h1>
-    <p class="text-gray-700">
+  <div class="even:bg-gray-100 p-4 sm:p-6">
+    <h1 class="font-bold text-xl sm:text-2xl break-words">{{ message.name }}</h1>
+    <p class="text-gray-700 mt-2 sm:mt-3 break-words">
       {{ message.message }}
     </p>
-    <div class="flex items-center mt-4">
-      <p class="text-green-700 mr-10">{{ message.email }}</p>
-      <p class="text-blue-500">{{ message.phone }}</p>
+    <div class="flex flex-col sm:flex-row sm:items-center sm:space-x-6 mt-3 sm:mt-4">
+      <p class="text-green-700 break-words">{{ message.email }}</p>
+      <p class="text-blue-500 break-words mt-1 sm:mt-0">{{ message.phone }}</p>
     </div>
   </div>
 </template>
